2bbc69a0b97d5611fbad8fc5ba1b549931536069
MrBrecken <79106179+MrBrecken@users.noreply.github.com>
2026-03-05 17:15:55 -0600

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.benroberts.dev/', // Public bare server
+    bare: 'https://bare.benroberts.dev/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.3/dist/uv.handler.js',
