9f8b231c9cb7b0572b239066e3766e2df888281b
MrBrecken <79106179+MrBrecken@users.noreply.github.com>
2026-03-05 18:22:20 -0600

    Update uv.sw.js

--- uv.sw.js
+++ uv.sw.js
@@ -1,14 +1,14 @@
-/* --- STEP 1: IMPORT NECESSARY SCRIPTS --- */
+/* --- STEP 1: IMPORT THE ENGINE --- */
 importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.3/dist/uv.bundle.js');
+
+/* --- STEP 2: IMPORT THE CONFIG --- */
 importScripts('/uv.config.js');
 
-/* --- STEP 2: WAIT AND INITIALIZE --- */
+/* --- STEP 3: INITIALIZE --- */
 if (self.Ultraviolet) {
     const sw = new self.UVServiceWorker();
 
     self.addEventListener('fetch', (event) => {
         event.respondWith(sw.fetch(event));
     });
-} else {
-    console.error("Ultraviolet failed to load inside the Service Worker.");
 }
