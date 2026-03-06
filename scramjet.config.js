f7c222f71609838eb39012f6a6ed70e483b54ea4
MrBrecken <79106179+MrBrecken@users.noreply.github.com>
2026-03-05 18:35:55 -0600

    Update and rename uv.config.js to scramjet.config.js

--- scramjet.config.js
+++ scramjet.config.js
@@ -1,7 +1,8 @@
+/* Scramjet Proxy Configuration */
 self.__scramjet$config = {
     prefix: '/service/',
-    codec: self.__scramjet$config?.codec || 'xor', // Uses XOR encryption for URLs
-    base: 'https://bare.asteris.xyz/', // You can use the same Bare servers
+    codec: 'xor', // Standard encryption for URLs
+    base: 'https://bare.asteris.xyz/', // Active Bare Server
     handler: 'https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@latest/dist/handler.js',
     bundle: 'https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@latest/dist/bundle.js',
     worker: '/sw.js',
